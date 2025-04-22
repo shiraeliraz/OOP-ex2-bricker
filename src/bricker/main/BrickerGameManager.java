801691ea21ad18010c15d35ea58417ff42d73c88
Shira Eliraz <shira.eliraz@gmail.com>
2025-04-22 15:54:01 +0300

    added ball to window

--- src/bricker/main/BrickerGameManager.java
+++ src/bricker/main/BrickerGameManager.java
@@ -1,4 +1,33 @@
 package bricker.main;
 
-public class BrickerGameManager {
+import danogl.GameManager;
+import danogl.GameObject;
+import danogl.gui.ImageReader;
+import danogl.gui.SoundReader;
+import danogl.gui.UserInputListener;
+import danogl.gui.WindowController;
+import danogl.gui.rendering.Renderable;
+import danogl.util.Vector2;
+
+public class BrickerGameManager extends GameManager {
+
+    public BrickerGameManager(String windowTitle, Vector2 windowDimensions) {
+        super(windowTitle, windowDimensions);
+    }
+
+    @Override
+    public void initializeGame(ImageReader imageReader, SoundReader soundReader, UserInputListener inputListener, WindowController windowController) {
+        super.initializeGame(imageReader, soundReader, inputListener, windowController);
+        Renderable ballImage =
+        imageReader.readImage("assets/ball.png", true);
+
+        GameObject ball = new GameObject(Vector2.ZERO, new Vector2(20,20), ballImage );
+        this.gameObjects().addGameObject(ball);
+    }
+
+    public static void main(String[] args) {
+        BrickerGameManager gameManager = new BrickerGameManager("Bricker", new Vector2(700,500));
+        gameManager.run();
+
+    }
 }
