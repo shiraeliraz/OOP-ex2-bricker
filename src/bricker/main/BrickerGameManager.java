779069f22521e7c15fd97fcfd7be7b1908cf9bff
ronishpitzer <roni.shpitzer@mail.huji.ac.il>
2025-04-22 16:01:50 +0300

    Ball is moving and in center

--- src/bricker/main/BrickerGameManager.java
+++ src/bricker/main/BrickerGameManager.java
@@ -22,7 +22,10 @@ public class BrickerGameManager extends GameManager {
         imageReader.readImage("assets/ball.png", true);
 
         GameObject ball = new GameObject(Vector2.ZERO, new Vector2(20,20), ballImage );
+        Vector2 windowDimension = windowController.getWindowDimensions();
+        ball.setCenter(windowDimension.mult(0.5f));
         this.gameObjects().addGameObject(ball);
+        ball.setVelocity(Vector2.DOWN.mult(100));
     }
 
     public static void main(String[] args) {
